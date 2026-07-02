c1356587f5fa177ca1afc593fb1fa25af861a28b
https://github.com/MohammadSajjadHossain9648 <mohammadsajjadhossain9648@gmail.com>
2026-07-02 15:05:36 +0600

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css'
+import NavBar from './components/NavBar/NavBar';
 
 const App = () => {
+
+
   return (
-    <div>
-      
+    <div className='max-w-[1100px] mx-auto'>
+      <NavBar></NavBar>
     </div>
   );
 };
