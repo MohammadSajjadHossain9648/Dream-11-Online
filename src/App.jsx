303e8de538924b5dae99c8c1993f0229264c3dc6
https://github.com/MohammadSajjadHossain9648 <mohammadsajjadhossain9648@gmail.com>
2026-07-03 00:32:19 +0600

    add react toastify functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers';
 import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers';
 import Footer from './components/Footer/Footer';
+import { toast, ToastContainer } from 'react-toastify';
 
 const fetchPlayers = async () => {
   const res = await fetch('/players.json');
@@ -13,19 +14,40 @@ const playersPromise = fetchPlayers();
 
 
 const App = () => {
+  const notify1 = (name) => {
+    toast.success(`Congrats to ${name}! Excited to see you shine!`, {
+      position:'top-center',
+      autoClose: 3000
+    });
+  }
+  const notify2 = () => {
+    toast.error(`no extra player allowed`, {
+      position: 'top-center',
+      autoClose: 3000
+    });
+  }
+  const notify3 = (name) => {
+    toast.error(`Remove player ${name}! See you next time!`, {
+      position:'top-center',
+      autoClose: 3000
+    });
+  }
+
+
   const [toggle, setToggle] = useState(true);
   const [cost, setCost] = useState(15000000);
 
 
   const [selectedPlayers, setSelectedPlayers] = useState([]);
   const handleToChoosePlayer = (player) => {
-    const {id, bidding_price} = player;
+    const {name, bidding_price} = player;
 
-    const isSelected = selectedPlayers.find(selectedPlayer => selectedPlayer.id === id);
-
-    if(isSelected || selectedPlayers.length === 12){
+    if(selectedPlayers.length === 12){
+      notify2();
       return ;
     }
+
+    notify1(name);
 
     setCost(cost-bidding_price);
 
@@ -35,7 +57,9 @@ const App = () => {
 
 
   const handleToRemovePlayer = (player) => {
-    const {id, bidding_price} = player;
+    const {id, name, bidding_price} = player;
+
+    notify3(name);
 
     const updateSelectedPLayers = selectedPlayers.filter(selectedPlayer => selectedPlayer.id !== id);
     setSelectedPlayers(updateSelectedPLayers);
@@ -55,10 +79,10 @@ const App = () => {
 
       <div className="flex justify-between my-8">
         {
-          toggle ? <h1 className="">Available Players</h1> : <h1 className="">Selected Players <span>({selectedPlayers.length}/12)</span></h1>
+          toggle ? <h1 className="font-bold">Available Players</h1> : <h1 className="font-bold">Selected Players <span>({selectedPlayers.length}/12)</span></h1>
         }
 
-        <div className="">
+        <div>
           <button onClick={() => setToggle(!toggle)} className={`btn rounded-l-3xl rounded-r-0 px-4 font-bold ${toggle ? 'bg-green-400' : ''}`}>Available</button>
           <button onClick={() => setToggle(!toggle)} className={`btn rounded-l-0 rounded-r-3xl px-4 font-bold ${toggle ? '' : 'bg-green-400'}`}>Selected<span>({selectedPlayers.length})</span></button>
         </div>
@@ -75,6 +99,8 @@ const App = () => {
       </div>
 
       <Footer></Footer>
+
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
